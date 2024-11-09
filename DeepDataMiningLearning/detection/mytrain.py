52c81b376261e8c3786705916006fc4c4dfa8499
Howell Ethan DeGuzman <013978029@coe-hpc1.sjsuad.sjsu.edu>
2024-11-08 17:48:16 -0800

    commented out modelevaluate for testing

--- DeepDataMiningLearning/detection/mytrain.py
+++ DeepDataMiningLearning/detection/mytrain.py
@@ -175,7 +175,7 @@ def main(args):
                 checkpoint["scaler"] = scaler.state_dict()
             utils.save_on_master(checkpoint, os.path.join(args.output_dir, f"model_{epoch + 1}.pth"))
             utils.save_on_master(checkpoint, os.path.join(args.output_dir, "checkpoint.pth"))
-            modelevaluate(model, data_loader_test, device=device)
+            # modelevaluate(model, data_loader_test, device=device)
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
