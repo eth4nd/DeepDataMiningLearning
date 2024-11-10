8817771609dca1cee16436cc7c6fa9b3fc06c0b1
Howell Ethan DeGuzman <013978029@coe-hpc1.sjsuad.sjsu.edu>
2024-11-10 14:09:12 -0800

    fix path for dataset_kitti

--- DeepDataMiningLearning/detection/dataset_kitti.py
+++ DeepDataMiningLearning/detection/dataset_kitti.py
@@ -50,7 +50,7 @@ class KittiDataset(torch.utils.data.Dataset):
         self.root = root
         self.train = train
         self.transform = transform
-        self._location = "training" if self.train else "testing"
+        self._location = "trai_subset" if self.train else "val_subset"
         self.image_dir_name = image_dir
         self.labels_dir_name = labels_dir
 
